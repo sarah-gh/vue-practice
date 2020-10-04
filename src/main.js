7e431bd6cfaf2f83addd696b9f0ab1d6b5d07bcb
sarah gh <sssara.2000.gh@gmail.com>
2020-10-04 12:47:54 +0330

    add project

--- src/main.js
+++ src/main.js
@@ -4,10 +4,26 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+    faPlus,
+    faMinus,
+    faTrash,
+    faCheck
+} from "@fortawesome/free-solid-svg-icons";
+
+// import { from } from "core-js/fn/array";
+
+library.add(faPlus, faMinus, faTrash, faCheck);
+
+
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<div class="home">
+    <h1 class="mb-2">{{ title }}</h1>
+    <FontAwesomeIcon icon="plus" class=" mr-2" /> Add Appointment
+    <hr>
+    <div v-for="(item, i ) in appointments" :key="i">
+        <h3> {{ appointments[i].petName }}</h3>
+        <p> {{ item.aptNotes }}</p>
+    </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import {
+    FontAwesomeIcon
+} from "@fortawesome/vue-fontawesome";
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    name: "Home",
+    data() {
+        return {
+            title: "Appointment List",
+            appointments: []
+        }
+    },
+    components: {
+        FontAwesomeIcon,
+    },
+    mounted() {
+        axios.get("./data/appointments.json")
+        .then(Response => (this.appointments = Response.data ))
+    },
 };
 </script>
