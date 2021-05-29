49a57004bfa08dc0260f4908bb122fc030cd7a27
sarah <sssara.2000.gh@gmail.com>
2021-05-29 23:49:45 +0430

    adding dist subtree

---
+++ precache-manifest.e8c4d25f7fc8c1e38f6d00529bd208fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54306b326262f2f44a6f",
+    "url": "/vue-practice/css/app.c262bede.css"
+  },
+  {
+    "revision": "66172fcebadea8870d8f",
+    "url": "/vue-practice/css/chunk-vendors.00ba962a.css"
+  },
+  {
+    "revision": "782192fef8bc56338137c6c35ea019f2",
+    "url": "/vue-practice/data/appointments.json"
+  },
+  {
+    "revision": "349160f2dfe687fbf8271d286b5f4b3b",
+    "url": "/vue-practice/index.html"
+  },
+  {
+    "revision": "dd703cefe843ebb60dcd",
+    "url": "/vue-practice/js/about.fde4075b.js"
+  },
+  {
+    "revision": "54306b326262f2f44a6f",
+    "url": "/vue-practice/js/app.4a6be23b.js"
+  },
+  {
+    "revision": "66172fcebadea8870d8f",
+    "url": "/vue-practice/js/chunk-vendors.c58cc6cd.js"
+  },
+  {
+    "revision": "5681e88a038a6cacf69911e8d28eafae",
+    "url": "/vue-practice/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-practice/robots.txt"
+  }
+]);
